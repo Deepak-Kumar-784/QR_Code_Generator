e6d20896efd303b20d1ae00b46b7ce9f0f5ff1b9
Deepak-Kumar-784 <deepakkumardipu659@gmail.com>
2025-11-28 00:16:41 +0530

    comment: clarify QR code generation with inline explanation

--- qr_code.py
+++ qr_code.py
@@ -4,16 +4,28 @@
 # )
 # img.save("wscube_youtube.jpeg")
 
-import qrcode
-from PIL import Image
+# Import the required libraries
+import qrcode  # Library to generate QR codes
+from PIL import Image  # Library to handle images (used by qrcode)
 
+# Create a QRCode object with customization options
 qr = qrcode.QRCode(
-    version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_H,
-    box_size=10,
-    border=4,
+    version=1,  # Controls the size of the QR code (1 = smallest)
+    error_correction=qrcode.constants.ERROR_CORRECT_H,  # High error correction (can recover from ~30% damage)
+    box_size=10,  # Size of each "box" in the QR code
+    border=4,  # Thickness of the border (default is 4)
 )
+
+# Add the URL or data you want to encode into the QR code
 qr.add_data("YOUR_URL_HERE")
-qr.make(fit=True)
-img = qr.make(fill_color="red", back_color="black")
+
+# Generate the QR code with the data added
+qr.make(fit=True)  # Adjust the QR code size automatically to fit the data
+
+# Create an image of the QR code with custom colors
+img = qr.make(
+    fill_color="red", back_color="black"
+)  # QR boxes in red, background in black
+
+# Save the QR code image as a file
 img.save("qr_code_image.jpeg")
